f6f6ff9aa6b7d7bc8b2682eed20fde961f7187dc
kanem <kanem.noel@gmail.com>
2022-01-10 15:34:04 -0500

    closes #3

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -10,6 +10,23 @@ export default function Player({ id, name, score, winner, active, inCurrentGame,
 
     const [diceRoll, setDiceRoll] = useState([]);
 
+    const scoreNames = {
+        13: "456",
+        12: "Trip 6",
+        11: "Trip 5",
+        10: "Trip 4",
+        9: "Trip 3",
+        8: "Trip 2",
+        7: "Trip 1",
+        6: "High 6",
+        5: "High 5",
+        4: "High 4",
+        3: "High 3",
+        2: "High 2",
+        1: "High 1",
+        [-1]: "123" 
+    }
+
     function rollDice() {
         let diceTemp = [];
         diceTemp.push(Math.floor(Math.random() * 6) + 1);
@@ -57,7 +74,7 @@ export default function Player({ id, name, score, winner, active, inCurrentGame,
     return (
         <div className="player-container" style={STYLE}>
             <h1>{name}</h1>
-            <h3>{score}</h3>
+            <h3>{scoreNames[score]}</h3>
             <Roll roll={diceRoll} />
             <button
                 onClick={rollDice}
